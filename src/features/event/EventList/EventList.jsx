fc0be76b77107785c80e3383f248ed21b0aec5d1
Arevik Hayrapetyan <arevhayrapetyan88@gmail.com>
2021-12-25 21:40:15 +0400

    feat: EventListItem

---
+++ src/features/event/EventList/EventList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Fragment } from "react/cjs/react.development";
+import EventListItem from "./EventListItem";
+
+export default function EventList() {
+  return (
+    <Fragment>
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+    </Fragment>
+  );
+}
---
+++ src/features/event/EventList/EventListItem.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button, Icon, Item, List, Segment } from "semantic-ui-react";
+
+export default function EventListItem() {
+  return (
+    <Segment.Group>
+      <Segment>
+        <Item.Group>
+          <Item>
+            <Item.Image size="tiny" circular src="" />
+            <Item.Content>
+              <Item.Header as="a">Event Title</Item.Header>
+              <Item.Description>
+                Hosted by <a>hosted by</a>
+              </Item.Description>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Segment>
+      <Segment>
+        <span>
+          <Icon name="clock" />
+          date
+          <Icon name="marker" />
+          time
+        </span>
+      </Segment>
+      <Segment secondary>
+        <List horizontal>{/* {todo:attendees} */}</List>
+      </Segment>
+      <Segment clearing>
+        <Button as="a" color="teal" floated="right" content="View"></Button>
+      </Segment>
+    </Segment.Group>
+  );
+}
