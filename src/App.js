f384ff559b7b82411ad641c4699e754c0c9bd090
Arevik Hayrapetyan <arevhayrapetyan88@gmail.com>
2022-01-28 19:37:28 +0400

    feat[App.js]: Routes path

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import { Fragment } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
 import { Container } from "semantic-ui-react";
 import "./App.css";
 import EventDashboard from "./features/event/EventDashboard/EventDashboard";
-import NavBar from "./features/nav/NavBar/NavBar";
+import HomePage from "./features/home/HomePage";
+import EventDetailedPage from "./features/event/EventDetailed/EventDetailedPage";
+import PeopleDashboard from "./features/user/PeopleDashboard/PeopleDashboard";
+import UserDetailedPage from "./features/user/UserDetailed/UserDetailedPage";
+import SettingsDashboard from "./features/user/Settings/SettingsDashboard";
+import EventForm from "./features/event/EventForm/EventForm";
 
+import NavBar from "./features/nav/NavBar/NavBar";
+// import { Switch } from "react-router";
 function App() {
   return (
     <Fragment>
       <NavBar />
       <Container className="main">
-        <EventDashboard />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/events" element={<EventDashboard />} />
+          <Route path="/events/:id" element={<EventDetailedPage />} />
+          <Route path="/people" element={<PeopleDashboard />} />
+          <Route path="/profile/:id" element={<UserDetailedPage />} />
+          <Route path="/settings" element={<SettingsDashboard />} />
+          <Route path="/createEvent" element={<EventForm />} />
+        </Routes>
       </Container>
     </Fragment>
   );
--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,17 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Button, Container, Menu } from "semantic-ui-react";
 
 export default function NavBar() {
   return (
     <Menu inverted fixed="top">
       <Container>
-        <Menu.Item header>
+        <Menu.Item header as={Link} to="/">
           <img src="assets/logo.png" alt="logo" />
           Blog
         </Menu.Item>
-        <Menu.Item name="Events" />
+        <Menu.Item name="Events" as={Link} to="/events" />
         <Menu.Item>
-          <Button floated="right" positive inverted content="Create Event" />
+          <Button
+            floated="right"
+            positive
+            inverted
+            content="Create Event"
+            as={Link}
+            to="/createEvent"
+          />
         </Menu.Item>
         <Menu.Item position="right">
           <Button basic inverted content="Login" />
